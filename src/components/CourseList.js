2e99f39b5a5c82bed10032409a7a3af8e23fbee4
Raajesh3108 <prajapatirajesh2003@gmail.com>
2024-03-12 11:35:19 +0530

    Initial commit

---
+++ src/components/CourseList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from "styled-components";
+import Tabs from "./Tabs";
+import { useCoursesContext } from '../context/courses_context';
+
+const CourseList = () => {
+  const {courses} = useCoursesContext();
+
+  return (
+    <CoursesListWrapper>
+      <div className='container'>
+        <div className='courses-list-top'>
+          <h2>An extensive selection of courses</h2>
+          <p>Choose from 6,500+ courses, Guided Projects, Specializations, and Professional Certificates with new additions publihsed every month</p>
+        </div>
+
+        <Tabs courses = {courses} />
+      </div>
+    </CoursesListWrapper>
+  )
+}
+
+const CoursesListWrapper = styled.div`
+  padding: 40px 0;
+  .courses-list-top p{
+    font-size: 1.8rem;
+  }
+`;
+
+export default CourseList
